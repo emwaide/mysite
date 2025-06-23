32687cbee5f444ff111f9b6c73d613501e54c494
emwaide <36140597+emwaide@users.noreply.github.com>
2025-06-23 22:24:37 +0000

    Resolved API route strict typing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-// app/page.tsx or app/home/page.tsx
 import Hero from '@/components/portfolio/Hero';
 import About from '@/components/portfolio/About';
 import ProjectList from '@/components/portfolio/ProjectList';
 import Contact from '@/components/portfolio/Contact';
+import { authOptions } from '@/lib/authOptions';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 export default async function HomePage() {
   const session = await getServerSession(authOptions);
---
+++ src/lib/authOptions.ts
@@ -0,0 +1,17 @@
+import GoogleProvider from 'next-auth/providers/google';
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+  },
+};
